b17e0bdc4eca067ae1234c306f6ac788568596a6
kkkk-oss <2765392548@qq.com>
2022-03-24 16:44:45 +0800

    first commit

---
+++ Employee-management-system/boss.h
@@ -0,0 +1,17 @@
+//老板文件
+#pragma once
+#include<iostream>
+using namespace std;
+#include"worker.h"
+
+class Boss : public Worker //继承抽象类
+{
+public:
+	//构造函数
+	Boss(int id, string name, int dId);
+	//显示个人信息
+	virtual void showInfo();
+	//获取岗位名称
+	virtual string getDeptName();
+
+};
